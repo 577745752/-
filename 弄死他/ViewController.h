6eb9cac4646d7715667fd5a10948f1692c7690f3
mac <577745752@qq.com>
2018-11-06 14:52:34 +0800

    搞起来

---
+++ 弄死他/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  弄死他
+//
+//  Created by mac on 2018/11/6.
+//  Copyright © 2018年 mac. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ 弄死他/ViewController.m
@@ -0,0 +1,23 @@
+//
+//  ViewController.m
+//  弄死他
+//
+//  Created by mac on 2018/11/6.
+//  Copyright © 2018年 mac. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+@end
